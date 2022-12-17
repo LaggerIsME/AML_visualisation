b6cf7042b45916ab521b6d9b4a7afe684a075d69
hecker <pythondelay@gmail.com>
2022-12-17 23:27:34 +0600

    task 2.1

--- main.py
+++ main.py
@@ -3,7 +3,44 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import math
 
-def graphs():
+
+def graph_word():
+    # Task 2.1: You need to output data on the schedule of teachers who teach disciplines and add a column "points
+    # for subjects"
+    df = pd.DataFrame([
+        ['John John', 'English', 90],
+        ['Joe Sam', 'English', 80],
+        ['Lars Jake', 'English', 70],
+        ['Carl Carl', 'Mathematics', 80],
+        ['Alexandr Lars', 'Mathematics', 95],
+        ['Joe Jake', 'Mathematics', 70],
+        ['Mary Mary', 'Russian', 100],
+        ['Mary Lars', 'Russian', 70],
+        ['Don Jey', 'Russian', 90],
+    ],
+        columns=['Full name', 'Subject', 'Points'])
+    plt.figure(figsize=(16, 9))
+    english_df = df.loc[df['Subject'] == 'English']
+    russian_df = df.loc[df['Subject'] == 'Russian']
+    mathematics_df = df.loc[df['Subject'] == 'Mathematics']
+    plt.bar(english_df['Full name'], english_df['Points'])
+    plt.bar(russian_df['Full name'], russian_df['Points'])
+    plt.bar(mathematics_df['Full name'], mathematics_df['Points'])
+    plt.grid()
+    plt.legend(labels=['English', 'Russian', 'Mathematics'])
+    plt.title('The Points of Teachers of 3 disciplines', fontsize=20, weight='bold')
+    plt.xlabel('Full names', fontsize=20)
+    plt.ylabel('Points', fontsize=20)
+    plt.show()
+
+    # Task 2.2: You need to deduce data on work experience, education, teaching methods of teachers
+    # who teach disciplines
+
+
+    # Task 2.3: You need to make a data output using all the graph types presented
+
+
+def graph_pdf():
     # Reading of CSV
     fig, axs = plt.subplots(2, 2, figsize=(16, 12))
     plt.suptitle('Assignment 2')
@@ -29,7 +66,7 @@ def graphs():
     # With hexbin plot
     ax3 = axs[1, 0]
     ax3.set_title('3)With hexbin plot')
-    hb = ax3.hexbin(x=longitude, y=latitude, bins = 'log', cmap='viridis')
+    hb = ax3.hexbin(x=longitude, y=latitude, bins='log', cmap='viridis')
     ax3.set_xlabel('longitude')
     ax3.set_ylabel('latitude')
     # Colorbar
@@ -48,7 +85,8 @@ def graphs():
 
 
 def main():
-    graphs()
+    # graph_pdf()
+    graph_word()
 
 
 if __name__ == '__main__':
