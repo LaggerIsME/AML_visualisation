472915eeb2c29fee6a5888c7844e9ca1a3d994e9
hecker <pythondelay@gmail.com>
2022-12-18 17:50:47 +0600

    task 2.2, task 2.3

--- main.py
+++ main.py
@@ -23,27 +23,93 @@ def graph_word():
     english_df = df.loc[df['Subject'] == 'English']
     russian_df = df.loc[df['Subject'] == 'Russian']
     mathematics_df = df.loc[df['Subject'] == 'Mathematics']
-    plt.bar(english_df['Full name'], english_df['Points'])
-    plt.bar(russian_df['Full name'], russian_df['Points'])
-    plt.bar(mathematics_df['Full name'], mathematics_df['Points'])
+    plt.bar(english_df['Full name'], english_df['Points'], width=1.0, edgecolor='black')
+    plt.bar(russian_df['Full name'], russian_df['Points'], width=1.0, edgecolor='black')
+    plt.bar(mathematics_df['Full name'], mathematics_df['Points'], width=1.0, edgecolor='black')
     plt.grid()
     plt.legend(labels=['English', 'Russian', 'Mathematics'])
     plt.title('The Points of Teachers of 3 disciplines', fontsize=20, weight='bold')
-    plt.xlabel('Full names', fontsize=20)
-    plt.ylabel('Points', fontsize=20)
+    plt.xlabel('Teachers', fontsize=20)
+    plt.ylabel('Points(%)', fontsize=20)
     plt.show()
 
     # Task 2.2: You need to deduce data on work experience, education, teaching methods of teachers
     # who teach disciplines
-
+    df = pd.DataFrame([
+        ['John John', 5, 'Phd', 'offline', 'English'],
+        ['Joe Sam', 7, 'Magistrate', 'online', 'English'],
+        ['Lars Jake', 12, 'Magistrate', 'offline', 'English'],
+        ['Carl Carl', 11, 'Phd', 'offline', 'Mathematics'],
+        ['Alexandr Lars', 13, 'Magistrate', 'offline', 'Mathematics'],
+        ['Joe Jake', 14, 'Magistrate', 'offline', 'Mathematics'],
+        ['Mary Mary', 17, 'Phd', 'online', 'Russian'],
+        ['Mary Lars', 18, 'Magistrate', 'online', 'Russian'],
+        ['Don Jey', 19, 'Magistrate', 'offline' 'Russian'],
+    ], columns=['Full name', 'Working Experience', 'Education', 'Teaching Method', 'Discipline'])
+    fig, axs = plt.subplots(2, 2, figsize=(16, 9))
+    plt.suptitle('Statistics of teachers')
+    # Working Experience
+    ax1 = axs[0, 0]
+    experience_less_than_10 = df[df['Working Experience'] < 10].count()[0]
+    experience_less_than_15 = df[df['Working Experience'] < 15].count()[0]
+    experience_more_than_15 = df[df['Working Experience'] >= 15].count()[0]
+    all_experience = [experience_less_than_10, experience_less_than_15, experience_more_than_15]
+    ax1.pie(all_experience, autopct='%1.1f%%')
+    ax1.set_title('Working Experience')
+    ax1.legend(['Experience less than 10',
+                'Experience less than 15',
+                'Experience equal or more than 15'],
+               loc='lower left', bbox_to_anchor=(-0.5, 0, 0.5, 0.5))
+    # Education
+    ax2 = axs[0, 1]
+    magistrate = df[df['Education'] == 'Magistrate'].count()[0]
+    phd = df[df['Education'] == 'Phd'].count()[0]
+    all_education = [magistrate, phd]
+    ax2.pie(all_education, autopct='%1.1f%%')
+    ax2.set_title('Education')
+    ax2.legend(['Magistrate',
+                'Phd'],
+               loc='lower left', bbox_to_anchor=(-0.5, 0, 0.5, 0.5))
+    # Teaching Method
+    ax3 = axs[1, 0]
+    offline = df[df['Teaching Method'] == 'offline'].count()[0]
+    online = df[df['Teaching Method'] == 'online'].count()[0]
+    all_teaching_methods = [offline, online]
+    ax3.pie(all_teaching_methods, autopct='%1.1f%%')
+    ax3.set_title('Teaching Method')
+    ax3.legend(['offline',
+                'online'],
+               loc='lower left', bbox_to_anchor=(-0.5, 0, 0.5, 0.5))
+    # Discipline
+    ax4 = axs[1, 1]
+    english = df[df['Discipline'] == 'English'].count()[0]
+    mathematics = df[df['Discipline'] == 'Mathematics'].count()[0]
+    russian = df[df['Discipline'] == 'Russian'].count()[0]
+    all_disciplines = [english, mathematics, russian]
+    ax4.pie(all_disciplines, autopct='%1.1f%%')
+    ax4.set_title('Discipline')
+    ax4.legend(['English',
+                'Mathematics',
+                'Russian'],
+               loc='lower left', bbox_to_anchor=(-0.5, 0, 0.5, 0.5))
+    plt.show()
 
     # Task 2.3: You need to make a data output using all the graph types presented
-
-
+    car = ['Daewoo', 'Mercedes', 'Samsung', 'Toyota', 'Honda']
+    explode = (0.05, 0.05, 0.05, 0.05, 0.05)
+    number_of_cars = [255000, 300000, 70000, 450000, 130000]
+    plt.pie(number_of_cars, labels=car,  explode=explode, autopct='%1.1f%%', pctdistance=0.80)
+    # Draw a Circle
+    centre = plt.Circle((0, 0), 0.70, fc='white')
+    fig = plt.gcf()
+    # Add in Pie Chart
+    fig.gca().add_artist(centre)
+    plt.title('The popularity of car brands in Kazakhstan')
+    plt.show()
 def graph_pdf():
     # Reading of CSV
     fig, axs = plt.subplots(2, 2, figsize=(16, 12))
-    plt.suptitle('Assignment 2')
+    plt.suptitle('Difference between longitude and latitude values')
     df = pd.read_csv('AML_DataVisualization.csv')
     longitude = df['longitude']
     latitude = df['latitude']
